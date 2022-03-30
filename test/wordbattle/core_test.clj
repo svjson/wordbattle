5479f3aac00d362beeb9c67f62c02fe3b0cf76bb
svjson <johansson.sven@gmail.com>
2022-03-30 19:59:38 +0200

    Updated tests

--- test/wordbattle/core_test.clj
+++ test/wordbattle/core_test.clj
@@ -1,43 +1,47 @@
 (ns wordbattle.core-test
-  (:require [clojure.test :refer :all]
-            [wordbattle.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [wordbattle.core :as core]))
 
 (deftest test-blank-out
-
   (testing "....."
-    (is (= (blank-out "BLAST" [false false false false false])
+    (is (= (core/blank-out "BLAST" [false false false false false])
            "BLAST")))
 
   (testing "TTTTT"
-    (is (= (blank-out "BLAST" [true true true true true])
+    (is (= (core/blank-out "BLAST" [true true true true true])
            "")))
 
   (testing ".T.T."
-    (is (= (blank-out "BLAST" [false true false true false])
+    (is (= (core/blank-out "BLAST" [false true false true false])
            "BAT"))))
 
 (deftest test-evaluate-guess
   (testing "SPACE vs SPARK"
-    (is (= (evaluate-guess "SPACE" "SPARK")
-           {:right-pos 3
+    (is (= (core/evaluate-guess "SPACE" "SPARK")
+           {:word "SPACE"
+            :right-pos 3
             :wrong-pos 0})))
 
   (testing "CUMIN vs CAROM"
-    (is (= (evaluate-guess "CUMIN" "CAROM")
-           {:right-pos 1
+    (is (= (core/evaluate-guess "CUMIN" "CAROM")
+           {:word "CUMIN"
+            :right-pos 1
             :wrong-pos 1})))
 
   (testing "GHOST vs PAPER"
-    (is (= (evaluate-guess "GHOST" "PAPER")
-           {:right-pos 0
+    (is (= (core/evaluate-guess "GHOST" "PAPER")
+           {:word "GHOST"
+            :right-pos 0
             :wrong-pos 0})))
 
   (testing "CUBIT vs CUBIT"
-    (is (= (evaluate-guess "CUBIT" "CUBIT")
-           {:right-pos 5
+    (is (= (core/evaluate-guess "CUBIT" "CUBIT")
+           {:word "CUBIT"
+            :right-pos 5
             :wrong-pos 0})))
 
   (testing "GLEAN vs ANGLE"
-    (is (= (evaluate-guess "GLEAN" "ANGLE")
-           {:right-pos 0
+    (is (= (core/evaluate-guess "GLEAN" "ANGLE")
+           {:word "GLEAN"
+            :right-pos 0
             :wrong-pos 5}))))
--- test/wordbattle/match_test.clj
+++ test/wordbattle/match_test.clj
@@ -1,24 +1,39 @@
 (ns wordbattle.match-test
-  (:require [clojure.test :refer :all]
-            [wordbattle.match :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [wordbattle.match :as match]))
 
 (deftest test-begin-match
   (let [match (atom {})]
-    (begin-match! match)
-    (is (= 5 (count (:word @match))))
+    (match/begin-match! match "SPACE")
+    (is (= "SPACE" (:word @match)))
     (is (= [] (:attempts @match)))
     (is (= :ongoing (:status @match)))))
 
 (deftest test-make-attempt!
-  (testing "false guess"
+  (testing "false first guess"
     (let [match (atom {})]
-      (begin-match! match)
-      (make-attempt! match "TRACK")
-      (is (= 1 (count (:attempts @match))))
+      (match/begin-match! match "SPACE")
+      (match/make-attempt! match "TRACK")
+      (is (= (:attempts @match)
+             [{:right-pos 2
+               :wrong-pos 0
+               :word "TRACK"}]))
       (is (= :ongoing (:status @match)))))
   (testing "correct guess"
     (let [match (atom {})]
-      (begin-match! match)
-      (make-attempt! match "SPACE")
-      (is (= 1 (count (:attempts @match))))
+      (match/begin-match! match "SPACE")
+      (match/make-attempt! match "SPACE")
+      (is (= (:attempts @match)
+             [{:right-pos 5
+               :wrong-pos 0
+               :word "SPACE"}]))
+      (is (= :won (:status @match)))))
+  (testing "correct second guess"
+    (let [match (atom {})]
+      (match/begin-match! match "SPACE")
+      (match/make-attempt! match "TRACK")
+      (match/make-attempt! match "SPACE")
+      (is (= (:attempts @match)
+             [{:right-pos 2 :wrong-pos 0 :word "TRACK"}
+              {:right-pos 5 :wrong-pos 0 :word "SPACE"}]))
       (is (= :won (:status @match))))))
